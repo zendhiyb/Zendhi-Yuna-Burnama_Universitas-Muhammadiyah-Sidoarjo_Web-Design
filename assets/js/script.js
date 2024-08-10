fd2bf1900c11065cbc7dc95df2c42fb4b304bbc8
Zendhi Yuna Burnama <zendhiyb10@gmail.com>
2024-08-10 20:59:35 +0700

    Normalize line endings

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var typed1 = new Typed('#judul', {
+    strings: ['Badan Eksekutif Mahasiswa'],
+    typeSpeed: 30,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: false,
+    startDelay: 500
+});
